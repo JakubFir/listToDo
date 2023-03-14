9a041e4fcfe55929b6c75c5c5b02998432bd9c70
Jakub <jakubfirlejczyk@gmail.com>
2023-03-14 18:14:33 +0100

    delete method

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,6 +1,7 @@
 package com.crud.tasks.service;
 
 
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +20,14 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task getTaskById(Long taskId) {
-        return repository.findById(taskId).orElseThrow();
+    public Task getTaskById(Long taskId) throws TaskNotFoundException {
+        return repository.findById(taskId).orElseThrow(TaskNotFoundException::new);
+    }
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+
+    public void deleteById(Long taskId) {
+        repository.deleteById(taskId);
     }
 }
