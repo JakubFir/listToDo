9b29dd7fea7cbdf47601450f0c60ee1a6c33c699
Jakub <jakubfirlejczyk@gmail.com>
2023-03-14 16:59:40 +0100

    creating controller

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,22 +1,32 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
     @GetMapping
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
 
     @GetMapping(path = "{taskId}")
-    public TaskDto getTask(@PathVariable("taskId") Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
+    public TaskDto getTaskById(@PathVariable("taskId") Long taskId) {
+        Task task = service.getTaskById(taskId);
+        return taskMapper.mapToTaskDto(task);
     }
 
     @DeleteMapping(path = "{taskId}")
@@ -28,6 +38,7 @@ public class TaskController {
     public TaskDto updateTask(TaskDto taskDto) {
         return new TaskDto(1L, "Edited test title", "Test content");
     }
+
     @PostMapping
     public void createTask(@RequestBody TaskDto taskDto) {
 
